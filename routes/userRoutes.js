9449c5f2a5cf4cc4f5b00f938f6ede9b34a4b4f0
Borlakunta Soniya <155159154+Soniya-321@users.noreply.github.com>
2025-01-09 15:25:52 +0530

    userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,7 @@ const userController = require('../controllers/userController');
 router.post('/users', userController.createUser);
 router.get('/users', userController.getAllUsers);
 router.post('/login', userController.loginUser);
+router.get('/verify', userController.verifyUser);
 router.get('/users/:id', userController.getUserById);
 router.put('/users/:id', userController.updateUser);
 router.delete('/users/:id', userController.deleteUser);
