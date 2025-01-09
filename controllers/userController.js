9352419f9c65c5d1a8f2aaa3c7069a55a249e75e
Borlakunta Soniya <155159154+Soniya-321@users.noreply.github.com>
2025-01-09 15:07:33 +0530

    userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 
 // Create a new user
 exports.createUser = async (req, res) => {
@@ -18,6 +20,43 @@ exports.createUser = async (req, res) => {
   User.create(newUser, (err, results) => {
     if (err) return res.status(500).json({ error: err.message });
     res.status(201).json({ message: 'User created successfully', userId: results.insertId });
+  });
+};
+
+
+// Login a user
+exports.loginUser = async (req, res) => {
+  const { mobileNumber, password } = req.body;
+
+  if (!mobileNumber || !password) {
+    return res.status(400).json({ message: 'Mobile number and password are required' });
+  }
+
+  User.getAll((err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+
+    // Find the user by mobile number
+    const user = results.find((user) => user.mobile_number === mobileNumber);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Compare passwords
+    bcrypt.compare(password, user.password, (err, isMatch) => {
+      if (err) return res.status(500).json({ error: err.message });
+
+      if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+      }
+
+      // Generate a JWT token
+      const token = jwt.sign({ id: user.id, mobileNumber: user.mobile_number }, 'your_jwt_secret', {
+        expiresIn: '1h', // Token expires in 1 hour
+      });
+
+      res.status(200).json({ message: 'Login successful', token });
+    });
   });
 };
 
